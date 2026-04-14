ad1fe1b41d5598fd0096a2f25e0e939c242edcf0
MaxEntz <maxime.entz@epitech.eu>
2026-04-15 00:27:10 +0200

    feat(Asset): adding the header file for the menu asset

---
+++ asset/include/asset_menu.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2026
+** GameBoyGame
+** File description:
+** asset_menu.h
+*/
+
+#ifndef ASSET_MENU_H_
+    #define ASSET_MENU_H_
+
+    #include "project_types.h"
+
+extern const UINT8 grass_tile[];
+extern const UINT8 wall_tile[];
+extern const UINT8 square_tile[];
+extern const UINT8 map[];
+
+#endif /* !ASSET_MENU_H_ */
