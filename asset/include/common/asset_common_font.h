afb743d37deac0ec661e5e0d9e68556749e3d8a4
Enzo Laugel <enzo.laugel@epitech.eu>
2026-04-21 21:18:08 +0200

    feat(sprites): basic characters sprites tiles

---
+++ asset/include/common/asset_common_font.h
@@ -0,0 +1,56 @@
+/*
+** EPITECH PROJECT, 2026
+** GameBoyGame
+** File description:
+** asset_common_font.h
+*/
+
+#ifndef ASSET_COMMON_FONT_H_
+    #define ASSET_COMMON_FONT_H_
+
+    #include "common/project_types.h"
+
+enum common_font_tile_e {
+    TILE_SPACE,
+    TILE_A,
+    TILE_B,
+    TILE_C,
+    TILE_D,
+    TILE_E,
+    TILE_F,
+    TILE_G,
+    TILE_H,
+    TILE_I,
+    TILE_J,
+    TILE_K,
+    TILE_L,
+    TILE_M,
+    TILE_N,
+    TILE_O,
+    TILE_P,
+    TILE_Q,
+    TILE_R,
+    TILE_S,
+    TILE_T,
+    TILE_U,
+    TILE_V,
+    TILE_W,
+    TILE_X,
+    TILE_Y,
+    TILE_Z,
+    TILE_0,
+    TILE_1,
+    TILE_2,
+    TILE_3,
+    TILE_4,
+    TILE_5,
+    TILE_6,
+    TILE_7,
+    TILE_8,
+    TILE_9,
+    TILE_COUNT
+};
+
+extern const UINT8 common_font_tiles[];
+
+#endif /* !ASSET_COMMON_FONT_H_ */
