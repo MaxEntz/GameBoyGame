3e26222f56e8f5f2c43973755f563a6a6e8dd1ca
MaxEntz <maxime.entz@epitech.eu>
2026-05-03 17:27:39 +0200

    feat(Asset): adding the bush asset for the lobby

--- asset/include/asset_lobby.h
+++ asset/include/asset_lobby.h
@@ -16,6 +16,7 @@ extern const UINT8 wall_tile[];
 extern const UINT8 wall_tile_limit[];
 extern const UINT8 wall_crossing_tile[];
 extern const UINT8 tronc_tile[];
+extern const UINT8 bush_tile[];
 extern const UINT8 square_tile[];
 extern const UINT8 player_tiles_front[];
 extern const UINT8 player_tile_front_move[];
