73aff563bd6b34672365f2b431b4ef4ab1cb8a69
MaxEntz <maxime.entz@epitech.eu>
2026-05-14 10:12:55 +0200

    fix(sprite_movement): refine sprite tile updates based on fps_counter conditions

--- src/mg1/tm_sprite_handle.c
+++ src/mg1/tm_sprite_handle.c
@@ -15,15 +15,14 @@ move_sprite_personage(OUT trap_memory_t *game)
     if (game->is_moving) {
         set_sprite_tile(0, sens + 0);
         set_sprite_tile(1, sens + 1);
-        if (game->fps_counter < 15) {
+        if (game->fps_counter < 15 || (game->fps_counter > 30 && game->fps_counter < 45)) {
             set_sprite_tile(2, sens + 4);
             set_sprite_tile(3, sens + 3);
         }
-        else if (game->fps_counter < 30) {
+        else if (game->fps_counter < 30 || (game->fps_counter > 45 && game->fps_counter < 60)) {
             set_sprite_tile(2, sens + 2);
             set_sprite_tile(3, sens + 5);
-        } else
-            game->fps_counter = 0;
+        }
     } else {
         set_sprite_tile(0, sens + 0);
         set_sprite_tile(1, sens + 1);
