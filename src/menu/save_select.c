fecf6e7a40a6095d77aa709177105ec4550bc6aa
Enzo Laugel <enzo.laugel@epitech.eu>
2026-05-16 12:58:49 +0200

    fix(save): compilation warnings fixed

--- src/menu/save_select.c
+++ src/menu/save_select.c
@@ -16,6 +16,7 @@ static UINT8 g_selected = 0;
 void
 save_select(OUT game_t *game)
 {
+    (void)game;
     set_bkg_data(0, 1, void_tile);
     set_bkg_tiles(0, 0, 20, 18, save_select_map);
     text_renderer_init();
